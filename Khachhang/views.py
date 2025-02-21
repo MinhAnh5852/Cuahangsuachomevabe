7cd8191653f519bb9a6e2db5b621c0d0234bc2a2
Phuongvy260905 <vynnp6064@ut.edu.vn>
2025-02-22 01:31:57 +0700

    code

--- Khachhang/views.py
+++ Khachhang/views.py
@@ -3,4 +3,4 @@ from . import views
 # Create your views here.
 
 def get_kh(request):
-    return render(request,'Khachhang/khachhang.html')
+    return render(request,'Khachhang/khachhang.html')                                                    
--- Sanpham/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from . import views
-
-# Create your views here.
-def sanpham(request):
-    return render(request,'Sanpham/sanpham.html')
